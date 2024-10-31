9064475f46df58db3f6941ccc50d2a25c547e6b4
Morgan OShaughnessey <123529383+theMorganObject@users.noreply.github.com>
2024-10-31 13:06:25 -1000

    styled nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,21 @@
-// src/components/Navbar.js
 import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
-          <Link href='/about'>About</Link>
-        </li>
-      </ul>
+    <nav className='w-full bg-gray-800'>
+      <div className='max-w-sm mx-auto px-4'>
+        <ul className='flex justify-between py-4 text-white'>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <Link href='/about'>About</Link>
+          </li>
+          <li>
+            <Link href='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
