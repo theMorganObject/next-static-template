a92f3754644708586c2f1d310cfc70f681a2d284
Morgan OShaughnessey <123529383+theMorganObject@users.noreply.github.com>
2024-11-06 11:48:09 -1000

    removed text-center from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-center min-h-screen p-4 text-center'>
+    <main className='flex flex-col items-center justify-center min-h-screen p-4'>
       <h1 className='text-4xl font-bold mb-4'>Welcome to My Template Site</h1>
 
       <Image
