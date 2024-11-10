2f0839287b5e84b19bfa5b467be3e99fb8977f0b
Morgan OShaughnessey <123529383+theMorganObject@users.noreply.github.com>
2024-11-10 08:04:54 -1000

    dynamic nav items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,21 @@
 import Link from 'next/link';
 
 export default function Navbar() {
+  const navItems = [
+    { href: '/', text: 'Home' },
+    { href: '/about', text: 'About' },
+    { href: '/contact', text: 'Contact' },
+  ];
+
   return (
     <nav className='w-full bg-gray-800'>
       <div className='max-w-sm mx-auto px-4'>
         <ul className='flex justify-between py-4 text-white'>
-          <li>
-            <Link href='/'>Home</Link>
-          </li>
-          <li>
-            <Link href='/about'>About</Link>
-          </li>
-          <li>
-            <Link href='/contact'>Contact</Link>
-          </li>
+          {navItems.map((item) => (
+            <li key={item.text}>
+              <Link href={item.href}>{item.text}</Link>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
